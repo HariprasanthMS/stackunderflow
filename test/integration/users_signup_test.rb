4d80d4b1fa408f3d1ca06a764dd5876aaa50adf8
hariprasanthMSH <hariprasanth.ms@chronus.com>
2021-05-24 14:13:28 +0530

    Finish sign-up work

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,36 @@
+require "test_helper"
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count', 'An user should not be created' do
+      post users_path, params: { user: { name:  "",
+                                         email: "user@invalid",
+                                         password:              "foo",
+                                         password_confirmation: "bar" } }
+    end 
+    assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
+    assert_select 'div.alert'
+    assert_select 'div.alert-danger'
+    assert_select 'div#error_explanation li'
+  end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1, 'An user should be created' do
+      post users_path, params: { user: { name:  "Example",
+                                         email: "example@example.com",
+                                         password:              "example",
+                                         password_confirmation: "example" } }
+    end 
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
+  end
+end

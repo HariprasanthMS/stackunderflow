260948a391e71ec71557ad1b795f749fef3e81cb
Hariprasanth <hariprasanth@chronus.com>
2022-07-27 18:46:46 +0530

    aws.rb file region mention directly

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,5 +1,5 @@
 require 'aws-sdk-s3' 
 Aws.config.update({    
-   region: ENV['AWS_S3_REGION'],    
+   region: 'us-east-1',    
    credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID_IAM'], ENV['AWS_SECRET_ACCESS_KEY_IAM'])  })
 S3_BUCKET = Aws::S3::Resource.new.bucket(ENV['ASSET_S3_BUCKET'])

5dabe220aab65ef3c7ec948523878fc8f7cfd785
Hariprasanth <hariprasanth@chronus.com>
2022-07-29 15:56:55 +0530

    error handle

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,13 +1,23 @@
 module UsersHelper
   def gravatar_for(user, size: 50)
     if user.profile_image.attached?
-        processed_key = user.profile_image.variant(resize: "#{size}x#{size}!").processed.key
-        full_cdn_url = "#{ENV['AWS_CLOUD_FRONT_URL']}/#{processed_key}"
-        image_tag(full_cdn_url, class: "image")
-    else 
-      gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-      gravatar_url = "#{ENV['GRAVATAR_URL']}/#{gravatar_id}"
-      image_tag(gravatar_url, alt: user.name, class: "gravatar", size: size, class: "image")
+        begin
+          raise
+            # block where exception raise
+            processed_key = user.profile_image.variant(resize: "#{size}x#{size}!").processed.key
+            full_cdn_url = "#{ENV['AWS_CLOUD_FRONT_URL']}/#{processed_key}"
+            image_tag(full_cdn_url, size: size, class: "image")
+          rescue
+            get_default_gravatar(user, size: size)
+        end
+    else
+      get_default_gravatar(user, size: size)
     end
+  end
+
+  def get_default_gravatar(user, size: 50)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "#{ENV['GRAVATAR_URL']}/#{gravatar_id}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar", size: size, class: "image")
   end
 end
